b47f2dcca0bd81ef2ba8fa98c2443c0306e8d25f
Kenny Chung <chunghlken@gmail.com>
2022-06-25 17:27:38 -1000

    setup keyboard

--- src/Components/Character.jsx
+++ src/Components/Character.jsx
@@ -1,9 +1,31 @@
-const Character = (prop) => {
-    return (
-        <div className="character">
-            <div>{prop.value ? prop.value.toUpperCase() : ' '}</div>
-        </div>
-    )
+import { Component } from "react";
+
+class Character extends Component {
+    state;
+    constructor(prop) {
+        super(prop);
+        this.state = { value: prop.value, type: prop.type };
+    }
+
+    handleInput(e) {
+        console.log(e.target.outerText);
+    }
+
+    render() {
+        if (this.state.type === 'button') {
+            return (
+                <div className="character">
+                    <button className="keyboard-button" onClick={this.handleInput}>{this.state.value ? this.state.value : ' '}</button>
+                </div>
+            )
+        } else {
+            return (
+                <div className="character">
+                    <div>{this.state.value ? this.state.value.toUpperCase() : ' '}</div>
+                </div>
+            )
+        }
+    }
 }
 
 export default Character;
--- src/Containers/KeyboardContainer.jsx
+++ src/Containers/KeyboardContainer.jsx
@@ -1,7 +1,33 @@
+import Character from "../Components/Character";
+
 const KeyboardContainer = (prop) => {
+
+    const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0',];
+    const lineOne = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'];
+    const lineTwo = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'];
+    const lineThree = ['Z', 'X', 'C', 'V', 'B', 'N', 'M'];
+
+    const numberLine = createElementArr(numbers);
+    const alphaLineOne = createElementArr(lineOne);
+    const alphaLineTwo = createElementArr(lineTwo);
+    const alphaLineThree = createElementArr(lineThree);
+
     return (
-        <div>KEYBOARD</div>
+        <div className="keyboardContainer">
+            <div className="lines">{numberLine}</div>
+            <div className="lines">{alphaLineOne}</div>
+            <div className="lines">{alphaLineTwo}</div>
+            <div className="lines">{alphaLineThree}</div>
+        </div>
     )
+}
+
+function createElementArr(arr) {
+    const elementArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        elementArr.push(<Character value={arr[i]} type="button" key={i} />)
+    }
+    return elementArr;
 }
 
 export default KeyboardContainer;
